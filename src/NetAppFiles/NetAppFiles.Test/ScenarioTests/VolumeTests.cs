be3cfe4744054ba3399ed2df2a331ae17c76161c
leonard <leonard.francis@netapp.com>
2020-02-03 08:31:25 +0000

    ANF-447 add cmdlets for 2019-10-01 API

--- src/NetAppFiles/NetAppFiles.Test/ScenarioTests/VolumeTests.cs
+++ src/NetAppFiles/NetAppFiles.Test/ScenarioTests/VolumeTests.cs
@@ -31,7 +31,6 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestVolumeCrud()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-VolumeCrud");
         }
 
@@ -39,7 +38,6 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestVolumeReplication()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-VolumeReplication");
         }
 
@@ -47,15 +45,7 @@ namespace Microsoft.Azure.Commands.NetAppFiles.Test.ScenarioTests.ScenarioTest
         [Trait(Category.AcceptanceType, Category.CheckIn)]
         public void TestVolumePipelines()
         {
-            GetSessionsDirectoryPath();
             TestController.NewInstance.RunPowerShellTest(_logger, "Test-VolumePipelines");
-        }
-
-        private static void GetSessionsDirectoryPath()
-        {
-            string connectionString = "SubscriptionId=69a75bda-882e-44d5-8431-63421204132a;ServicePrincipal=c6c4faba-2b22-44d9-80a4-71ff5b71f811;ServicePrincipalSecret={QR[+@avhE+3qiyY1Xf;AADTenant=72f988bf-86f1-41af-91ab-2d7cd011db47;Environment=Prod;HttpRecorderMode=Record;";
-            System.Environment.SetEnvironmentVariable("TEST_CSM_ORGID_AUTHENTICATION", connectionString, System.EnvironmentVariableTarget.Process);
-            System.Environment.SetEnvironmentVariable("AZURE_TEST_MODE", "Record", System.EnvironmentVariableTarget.Process);
         }
     }
 }
